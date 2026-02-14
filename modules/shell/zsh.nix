bde4e3ade6c888b3ba885aa471714b9709663668
lsdhophora <lsdphophora@proton.me>
2026-02-14 18:39:14 +0800

    shell: add nixos-assistant alias for opencode session

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -17,6 +17,13 @@
     interactiveShellInit = ''
       setopt NO_PROMPT_CR
       setopt no_beep
+
+      nixos-assistant() {
+        local orig_dir="$(pwd)"
+        cd ~/.config/nixos
+        opencode -s ses_3a4bcecf7ffemi02sJ5poVS1vw
+        cd "$orig_dir"
+      }
     '';
   };
 
