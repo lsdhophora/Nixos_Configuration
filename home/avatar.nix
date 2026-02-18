f448fda10e8f21ec05199b3f0f245ca16e7016dc
lsdhophora <lsdphophora@proton.me>
2026-02-19 01:28:53 +0800

    Use pkgs.dbus instead of hardcoded path

--- home/avatar.nix
+++ home/avatar.nix
@@ -6,6 +6,6 @@
   };
 
   home.activation.setAvatar = lib.hm.dag.entryAfter [ "writeBorder" ] ''
-    /run/current-system/sw/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.face
+    ${pkgs.dbus}/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.face
   '';
 }
