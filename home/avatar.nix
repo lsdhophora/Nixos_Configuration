d4e1e892623631fb63809db363c618fce19834d5
lsdhophora <lsdphophora@proton.me>
2026-02-19 01:18:01 +0800

    Remove dbus-send from activation script

--- home/avatar.nix
+++ home/avatar.nix
@@ -4,10 +4,4 @@
   home.file.".face" = {
     source = ../assets/avatar/face.png;
   };
-
-  home.activation.setAvatar = lib.hm.dag.entryAfter [ "writeBorder" ] ''
-    if [ -f "$HOME/.face" ]; then
-      dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.face
-    fi
-  '';
 }
