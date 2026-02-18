1b15e07f4cae79e173628e0b67b9d69d1d02bea4
lsdhophora <lsdphophora@proton.me>
2026-02-19 01:16:26 +0800

    Remove sudo from dbus-send in activation script

--- home/avatar.nix
+++ home/avatar.nix
@@ -7,7 +7,7 @@
 
   home.activation.setAvatar = lib.hm.dag.entryAfter [ "writeBorder" ] ''
     if [ -f "$HOME/.face" ]; then
-      sudo dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.face
+      dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.face
     fi
   '';
 }
