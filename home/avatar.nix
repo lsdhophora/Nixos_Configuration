c54d0f6ae80bbc4ea7a2cdc62fbf393a8c32edae
lsdhophora <lsdphophora@proton.me>
2026-02-19 01:42:20 +0800

    Set avatar directly in SetIconFile path

--- home/avatar.nix
+++ home/avatar.nix
@@ -1,11 +1,7 @@
 { lib, pkgs, ... }:
 
 {
-  home.file.".face" = {
-    source = ../assets/avatar/face.png;
-  };
-
   home.activation.setAvatar = lib.hm.dag.entryAfter [ "writeBorder" ] ''
-    ${pkgs.dbus}/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:/home/lophophora/.face
+    ${pkgs.dbus}/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:/home/lophophora/.config/nixos/assets/avatar/face.png
   '';
 }
