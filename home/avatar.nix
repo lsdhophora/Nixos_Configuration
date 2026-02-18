4373672df4a64fb4630bdb5c95e674b249e18d94
lsdhophora <lsdphophora@proton.me>
2026-02-19 01:43:52 +0800

    Use $HOME instead of hardcoded path

--- home/avatar.nix
+++ home/avatar.nix
@@ -2,6 +2,6 @@
 
 {
   home.activation.setAvatar = lib.hm.dag.entryAfter [ "writeBorder" ] ''
-    ${pkgs.dbus}/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:/home/lophophora/.config/nixos/assets/avatar/face.png
+    ${pkgs.dbus}/bin/dbus-send --system --dest=org.freedesktop.Accounts --type=method_call --print-reply=literal /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User.SetIconFile string:$HOME/.config/nixos/assets/avatar/face.png
   '';
 }
