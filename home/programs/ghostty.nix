093b01a2ba287fbfa1b80368bb08320b0300e098
lsdhophora <lsdphophora@proton.me>
2026-02-27 19:48:55 +0800

    Use Maple Mono NF CN as Ghostty font

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -23,10 +23,7 @@
       "cursor-style" = "block";
       "shell-integration-features" = "no-cursor,title";
       "bell-features" = "no-title,attention";
-      "font-family" = [
-        "Adwaita Mono"
-        "Noto Sans CJK SC"
-      ];
+      "font-family" = [ "Maple Mono NF CN" ];
       "link-url" = false;
       "working-directory" = "inherit";
       "right-click-action" = "ignore";
