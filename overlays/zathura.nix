c4ebd42dba90a2c524735733f1e6b83228d3fda0
lsdhophora <lsdphophora@proton.me>
2026-03-07 04:58:47 +0800

    chore: remove unused zathura overlay

--- overlays/zathura.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  zathura = prev.zathura.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or [ ]) ++ [
-      ../patches/zathura-print-status-changed.patch
-    ];
-  });
-}
