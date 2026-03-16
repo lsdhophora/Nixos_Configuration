e2145eb46a6b7fd51bb0c8eaa25c2672431c2240
lsdhophora <lsdphophora@proton.me>
2026-03-17 00:25:43 +0800

    fix(overlays): use fetchCargoVendor for cargo deps

--- overlays/papers.nix
+++ overlays/papers.nix
@@ -8,6 +8,17 @@ final: prev: {
       rev = "50.0";
       hash = "sha256-3pus4MDgg0GWI5ayHRA+zS6JxXX+W8yjG/Un21GXRGo=";
     };
-    cargoHash = "sha256-6Fd6V0Ksl8jqoM1znyYI0Mve2QQU+JBf3yn2C2Bcda8=";
+    cargoDeps = prev.rustPlatform.fetchCargoVendor {
+      pname = "papers";
+      version = "50.0";
+      src = prev.fetchFromGitLab {
+        domain = "gitlab.gnome.org";
+        owner = "GNOME";
+        repo = "papers";
+        rev = "50.0";
+        hash = "sha256-3pus4MDgg0GWI5ayHRA+zS6JxXX+W8yjG/Un21GXRGo=";
+      };
+      hash = "sha256-6Fd6V0Ksl8jqoM1znyYI0Mve2QQU+JBf3yn2C2Bcda8=";
+    };
   });
 }
