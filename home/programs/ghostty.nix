b03f51d8a25acd9cf088e53f3f0788395ff960f9
lsdhophora <lsdphophora@proton.me>
2026-03-18 17:08:57 +0800

    feat(ghostty): update bell-features and add font-size

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -14,8 +14,9 @@
       theme = "Adwaita Dark";
       "cursor-style" = "block";
       "shell-integration-features" = "no-cursor,title";
-      "bell-features" = "no-title,attention";
+      "bell-features" = "no-title,no-attention,system";
       "font-family" = [ "Maple Mono NF CN" ];
+      "font-size" = "14";
       "link-url" = false;
       "right-click-action" = "ignore";
       "resize-overlay" = "never";
