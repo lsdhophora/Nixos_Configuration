7b4e53952062ccfa70bd6dce1f6bffb4c819c2c3
lsdhophora <lsdphophora@proton.me>
2026-06-05 22:38:55 +0800

    Replace emblem-ok-symbolic with object-select-symbolic in gjs-osk menu (not in Adwaita 50)

--- overlays/gjs-osk.nix
+++ overlays/gjs-osk.nix
@@ -8,10 +8,13 @@ final: prev: {
         rev = "4191527db5ca206547d48242ad4a08b10a5e3d03";
         hash = "sha256-hTqM0s4jIP59SMnVPn3NSyxhk8LkMYsyzNPN2Vgjrkw=";
       };
-      patches = [ ];
       postUnpack = ''
         cd "$sourceRoot/gjsosk@vishram1123.com"
         sourceRoot="$PWD"
+      '';
+      postPatch = ''
+        substituteInPlace extension.js \
+          --replace-fail 'emblem-ok-symbolic' 'object-select-symbolic'
       '';
     });
   };
