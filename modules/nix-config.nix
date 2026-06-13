a5a8c215b54aaf879b94653ffa9e248064f421a5
lsdhophora <lsdphophora@proton.me>
2026-06-13 17:18:23 +0800

    chore(nix-config): reorder substituters
    Place cache.nixos.org before nyx-cache.chaotic.cx

--- modules/nix-config.nix
+++ modules/nix-config.nix
@@ -5,12 +5,12 @@
     experimental-features = [ "nix-command" "flakes" ];
     trusted-users = [ "root" "lophophora" ];
     substituters = [
-      "https://nyx-cache.chaotic.cx/"
       "https://cache.nixos.org"
+      "https://nyx-cache.chaotic.cx/"
     ];
     trusted-public-keys = [
-      "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
     ];
   };
 }
