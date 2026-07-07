4bc7353d85688f2c6c9dcb2923ce6407fe3259ba
lsdhophora <lsdphophora@proton.me>
2026-07-08 00:44:32 +0800

    sway(mpv): change border to pixel

--- assets/sway/mpv-border.sh
+++ assets/sway/mpv-border.sh
@@ -2,6 +2,6 @@
 killall -q mpv-border.sh 2>/dev/null
 swaymsg -t subscribe -m '["window"]' | while read -r line; do
   if echo "$line" | jq -e '.container.app_id == "mpv"' > /dev/null 2>&1; then
-    swaymsg '[app_id="mpv"] border normal 2'
+    swaymsg '[app_id="mpv"] border pixel 2'
   fi
 done
