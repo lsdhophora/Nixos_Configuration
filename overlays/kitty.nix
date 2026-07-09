ee31c8626d26901ca9195484fa0d88c438030888
lsdhophora <lsdphophora@proton.me>
2026-07-09 10:19:32 +0800

    Fix(kitty): Correcting IME Panel Positions when switch workspaces

--- overlays/kitty.nix
+++ overlays/kitty.nix
@@ -2,6 +2,7 @@ final: prev: {
   kitty = prev.kitty.overrideAttrs (old: {
     patches = (old.patches or [ ]) ++ [
       ../patches/kitty/kitty-remove-resize-text.patch
+      ../patches/kitty/kitty-fix-panel-position.patch
     ];
   });
 }
