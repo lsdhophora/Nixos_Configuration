4da9fd05963420a338e58ea838f38430c611eb64
lsdhophora <lsdphophora@proton.me>
2026-08-08 08:57:40 +0800

    dae: switch subscription to flybit, rename sub to flybit

--- modules/services/dae.nix
+++ modules/services/dae.nix
@@ -17,7 +17,7 @@
       }
 
       subscription {
-        mojie: '${config.sops.placeholder."dae-subscription"}'
+        flybit: '${config.sops.placeholder."dae-subscription"}'
       }
 
       dns {
